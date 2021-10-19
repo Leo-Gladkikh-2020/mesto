9df68df49da9902cd2acfd2ea1607f2ed74f6b60
Leonid Gladkikh <leogladkikh@yandex.ru>
2021-10-19 19:40:33 +0200

    hot: исправлены замечания после первого ревью

--- scripts/index.js
+++ scripts/index.js
@@ -4,17 +4,20 @@ const editButton = document.querySelector('.profile__edit-btn');
 const closeButton = document.querySelector('.popup__close-btn');
 const profileTitle = document.querySelector('.profile__name');
 const profileSubtitle = document.querySelector('.profile__subtitle');
-const inputName = document.querySelector('.popup__input_name');
-const inputAbout = document.querySelector('.popup__input_about');
+const inputName = document.querySelector('.popup__input_text_name');
+const inputAbout = document.querySelector('.popup__input_text_about');
 
+// Функция открытия popup
 function openPopup() {
-    popup.classList.add('popup_isOpen')
+    popup.classList.add('popup_opened')
 }
 
+// Функция закрытия popup
 function closePopup() {
-    popup.classList.remove('popup_isOpen')
+    popup.classList.remove('popup_opened')
 }
 
+//Функция сохранения popup
 function submitForm(event) {
     event.preventDefault();
     profileTitle.textContent = inputName.value;
@@ -22,12 +25,13 @@ function submitForm(event) {
     closePopup();
 }
 
-function inputNameForm() {
+//Функция сохранения данных
+function saveNameForm() {
     openPopup()
     inputName.value = profileTitle.textContent;
     inputAbout.value = profileSubtitle.textContent;
 }
 
 form.addEventListener('submit', submitForm);
-editButton.addEventListener('click', inputNameForm);
+editButton.addEventListener('click', saveNameForm);
 closeButton.addEventListener('click', closePopup);
