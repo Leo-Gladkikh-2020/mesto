f481b8962c242c04fcc2a0a09f8e2a029cf9d7e4
Leonid Gladkikh <leogladkikh@yandex.ru>
2022-01-08 15:01:07 +0200

    исправить замечания ревьюера

--- src/components/Api.js
+++ src/components/Api.js
@@ -18,29 +18,29 @@ export class Api {
             .then(this._checkStatus)
     }
 
-    changeUserInfo({ name, about }) {
+    changeUserInfo(data) {
         return fetch(`${this._baseUrl}/users/me`, {
             method: 'PATCH',
             headers: this._headers,
-            body: JSON.stringify({ name, about })
+            body: JSON.stringify({ name: data.name, about: data.about })
         })
             .then(this._checkStatus)
     }
 
-    changeUserAvatar({ avatar }) {
+    changeUserAvatar(data) {
         return fetch(`${this._baseUrl}/users/me/avatar`, {
             method: 'PATCH',
             headers: this._headers,
-            body: JSON.stringify({ avatar })
+            body: JSON.stringify({ avatar: data.avatar })
         })
             .then(this._checkStatus)
     }
 
-    addCard({ name, link }) {
+    addCard(data) {
         return fetch(`${this._baseUrl}/cards`, {
             method: 'POST',
             headers: this._headers,
-            body: JSON.stringify({ name, link })
+            body: JSON.stringify({ name: data.title, link: data.link })
         })
             .then(this._checkStatus)
     }
